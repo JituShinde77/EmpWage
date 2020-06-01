9f6c2fb2f38de06c0b7b7881a4060c269122eaa1
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-02 00:09:04 +0530

    Added: Calculate Daily Wage Functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,14 @@
 echo "Welcome to Employee Wage Computation"
 
+# Constants
+WAGE_PER_HOURS=20
+WORKING_HOURS=8
+
 isPresent=$(( RANDOM % 2 ))
 if [ $isPresent -eq 1 ]
 then
 	echo "Present"
+	dilyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
 elif [ $isPresent -eq 0 ]
 then
 	echo "Absent"
