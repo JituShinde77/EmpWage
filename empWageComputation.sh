d17692a8fac74d3e5cc3bd2a07e4c601b2c587a1
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-02 00:48:07 +0530

    Added: Wages Month Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,25 +1,38 @@
 echo "Welcome to Employee Wage Computation"
 
 # Constants
-WAGE_PER_HOURS=20
-WORKING_HOURS=8
-PART_TIME_HOURS=4
 ABSENT=0
 PRESENT=1
 PART_TIME=2
+PART_TIME_HOURS=4
+WAGE_PER_HOURS=20
+WORKING_DAYS_PER_MONTH=20
+WORKING_HOURS=8
+PART_TIME_HOURS=4
 
-isPresent=$(( RANDOM % 3 ))
+#VARIABLES
+totalWage=0
 
-case $isPresent in
-	$PRESENT)
-	echo "Present"
-	dilyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
-	;;
-	$PART_TIME)
-	echo "Part time"
-	dailyWage=$(( $WAGE_PER_HOURS * $PART_TIME_HOURS ))
-	;;
-	$ABSENT)
-	echo "Absent"
-	;;
-esac
+for (( counter=0; counter<$WORKING_DAYS_PER_MONTH; counter++ ))
+do
+	isPresent=$(( RANDOM % 3 ))
+
+	case $isPresent in
+		$PRESENT)
+		echo "Present"
+		dailyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
+		totalWage=$(( $totalWage + $dailyWage ))
+		;;
+		$PART_TIME)
+		echo "Part time"
+		dailyWage=$(( $WAGE_PER_HOURS * $PART_TIME_HOURS ))
+		totalWage=$(( $totalWage + $dailyWage ))
+		;;
+		$ABSENT)
+		echo "Absent"
+		;;
+	esac
+
+done
+
+echo $totalWage
