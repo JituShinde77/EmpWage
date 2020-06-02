ffda9f999758f9a1ab6fd44321616e7f92997bb2
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-02 10:36:44 +0530

    Added: Storing daily wage and total Wage with storage functionality.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -13,8 +13,8 @@ totalWorkingHrs=0
 totalWorkingDays=0
 cnt=0
 
-declare -a dailyWage
-declare -a totalDailyWage
+declare -A dailyWage
+declare -A totalDailyWage
 
 #Function for get working hrs
 function getWorkHrs(){
@@ -37,22 +37,18 @@ function getWorkHrs(){
 while [[ $totalWorkingHrs -lt 100 && $totalWorkingDays -lt 20 ]]
 do
         isPresent=$(( RANDOM % 3 ))
-        #((totalWorkingDays++))
+        ((totalWorkingDays++))
 	workingHrs=$( getWorkHrs $isPresent )
-        dailyWage[totalWorkingDays]=$(( $WAGE_PER_HOURS * $workingHrs ))
+        dailyWage["Day "$totalWorkingDays]=$(( $WAGE_PER_HOURS * $workingHrs ))
 	totalWage=$(( $totalWage + ($WAGE_PER_HOURS * $workingHrs) ))
-	totalDailyWage[$totalWorkingDays]=$totalWage
+	totalDailyWage["Day "$totalWorkingDays]=$totalWage
         totalWorkingHrs=$(( $totalWorkingHrs + $workingHrs ))
-	((totalWorkingDays++))
 
 done
 
-for (( i=0; i<$totalWorkingDays; i++ ))
+echo "Day No.	    DailyWage	    TotalWage"
+
+for (( i=1; i<=$totalWorkingDays; i++ ))
 do
-	echo ${dailyWage[$i]}" "${totalDailyWage[$i]}
+	echo " Day "$i"		"${dailyWage["Day "$i]}"		"${totalDailyWage["Day "$i]}
 done
-
-echo $totalWage
-echo ${dailyWage[@]}
-
-echo ${totalDailyWage[@]}
