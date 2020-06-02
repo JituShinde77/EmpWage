707672c41815d0a7b0fa8a76e9926d15f0762967
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-02 08:24:04 +0530

    Added: Working Hours Function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,35 +7,39 @@ PART_TIME=2
 PART_TIME_HOURS=4
 WAGE_PER_HOURS=20
 WORKING_DAYS_PER_MONTH=20
-WORKING_HOURS=8
-PART_TIME_HOURS=4
+
 
 #VARIABLES
 totalWage=0
 totalWorkingHrs=0
 totalWorkingDays=0
 
+#Function for get working hrs
+function getWorkHrs(){
+
+	local empCheck=$1
+	case $empCheck in
+		$PRESENT)
+			empHrs=8
+		;;
+		$PART_TIME)
+			empHrs=4
+		;;
+		$ABSENT)
+			empHrs=0
+		;;
+	esac
+	echo $empHrs
+}
+
 while [[ $totalWorkingHrs -lt 100 && $totalWorkingDays -lt 20 ]]
 do
         isPresent=$(( RANDOM % 3 ))
         ((totalWorkingDays++))
-        case $isPresent in
-                $PRESENT)
-                echo "Present"
-                dailyWage=$(( $WAGE_PER_HOURS * $WORKING_HOURS ))
-                totalWage=$(( $totalWage + $dailyWage ))
-                totalWorkingHrs=$(( $totalWorkingHrs + $WORKING_HOURS ))
-                ;;
-                $PART_TIME)
-                echo "Part time"
-                dailyWage=$(( $WAGE_PER_HOURS * $PART_TIME_HOURS ))
-                totalWage=$(( $totalWage + $dailyWage ))
-                totalWorkingHrs=$(( $totalWorkingHrs + $WORKING_HOURS ))
-                ;;
-                $ABSENT)
-                echo "Absent"
-                ;;
-        esac
+	 workingHrs=$( getWorkHrs $isPresent )
+         dailyWage=$(( $WAGE_PER_HOURS * $workingHrs ))
+         totalWage=$(( $totalWage + $dailyWage ))
+         totalWorkingHrs=$(( $totalWorkingHrs + $workingHrs ))
 
 done
 
